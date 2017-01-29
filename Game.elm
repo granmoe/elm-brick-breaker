712b12c273c570f14cd41dd95ebe927da02821bc
Matt Granmoe <mattgranmoe@gmail.com>
2017-01-28 19:59:11 -0600

    Changed keyCode logic to the keys we need. Deleted incrementShotsFired and just return initial model in its place (R is the restart button)

--- Game.elm
+++ Game.elm
@@ -16,17 +16,12 @@ main =
         , subscriptions = subscriptions
         }
 
-
-
 -- MODEL
-
-
 type alias Model =
     { velocity : Float
     , position : Float
     , shotsFired : Int
     }
-
 
 model : Model
 model =
@@ -35,92 +30,65 @@ model =
     , shotsFired = 0
     }
 
-
 init : ( Model, Cmd Msg )
 init =
     ( model, Cmd.none )
 
 
-
 -- UPDATE
-
-
 type Msg
     = TimeUpdate Time
     | KeyDown KeyCode
     | KeyUp KeyCode
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         TimeUpdate dt ->
             ( applyPhysics dt model, Cmd.none )
-
         KeyDown keyCode ->
             ( keyDown keyCode model, Cmd.none )
-
         KeyUp keyCode ->
             ( keyUp keyCode model, Cmd.none )
-
 
 keyDown : KeyCode -> Model -> Model
 keyDown keyCode model =
     case Key.fromCode keyCode of
-        Space ->
-            incrementShotsFired model
-
+        R ->
+            Tuple.first init
         ArrowLeft ->
             updateVelocity -1.0 model
-
         ArrowRight ->
             updateVelocity 1.0 model
-
         _ ->
             model
-
 
 keyUp : KeyCode -> Model -> Model
 keyUp keyCode model =
     case Key.fromCode keyCode of
         ArrowLeft ->
             updateVelocity 0 model
-
         ArrowRight ->
             updateVelocity 0 model
-
         _ ->
             model
-
 
 applyPhysics : Float -> Model -> Model
 applyPhysics dt model =
     { model | position = model.position + model.velocity * dt }
-
 
 updateVelocity : Float -> Model -> Model
 updateVelocity newVelocity model =
     { model | velocity = newVelocity }
 
 
-incrementShotsFired : Model -> Model
-incrementShotsFired model =
-    { model | shotsFired = model.shotsFired + 1 }
-
-
-
 -- VIEW
-
-
 view : Model -> Html msg
 view model =
     text (toString model)
 
 
-
 -- SUBSCRIPTIONS
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
--- Key.elm
+++ Key.elm
@@ -1,24 +1,19 @@
 module Key exposing (..)
 
-
 type Key
-    = Space
-    | ArrowLeft
+    = ArrowLeft
     | ArrowRight
+    | R
     | Unknown
-
 
 fromCode : Int -> Key
 fromCode keyCode =
     case keyCode of
-        32 ->
-            Space
-
         37 ->
             ArrowLeft
-
         39 ->
             ArrowRight
-
+        82 ->
+            R
         _ ->
             Unknown
